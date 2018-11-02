f479f6d0510d78bfcb76210a4913d3a6ed01ecbe
Angela Wolff <warriorwomenblog@gmail.com>
2018-11-02 20:14:44 +0000

    Creating hashed passwords with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
+const _ = require('lodash');
 const {User, validate} = require('../models/users');
 const express = require('express');
+const bcrypt = require('bcrypt');
 // const mongoose = require('mongoose');
 const router = express.Router();
 
@@ -10,14 +12,12 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered");
   
-  user = new User({ 
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
-  
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
-  res.send(user);
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 
